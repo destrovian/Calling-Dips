1b447c2426f1fdc7b0e68bfa0d88ade92d700b40
destrovian <dario.zehnder@outlook.com>
2024-11-03 09:50:02 +0100

    testing a new version using airflow instead of py container + portainer

--- load-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Load .env file into the environment
-export $(grep -v '^#' .env | xargs)
-
-# Then you can run your commands here, e.g., docker build
-docker-compose build
-docker-compose up -d
